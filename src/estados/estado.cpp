e44e4c8bac240497ee9ab17c2217c3637da519ca
Gustavo Aguilar <gustavogoetten@alunos.utfpr.edu.br>
2023-11-02 12:50:10 -0300

    commit

--- src/estados/estado.cpp
+++ src/estados/estado.cpp
@@ -1,2 +1,21 @@
 
-#include "../includes/estados/"
+#include "../includes/estados/estado.h"
+
+namespace  Game{
+
+    namespace Estado{
+
+        Estado::Estado(IDs::IDs ID):
+        Ente(ID)
+        {
+            remover = false;
+        }
+
+        Estado::~Estado()
+        {
+
+        }
+
+        
+    }
+}
